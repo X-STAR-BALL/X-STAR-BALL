91d3aae164f64f641b9924eb91110df266648d6d
X-STAR-BALL <rinaudocloe@gmail.com>
2024-04-21 18:46:34 +0200

    Ajout des programmes déjà existants

---
+++ Arduino Uno/bluetooth.h
@@ -0,0 +1,12 @@
+/**************************************************
+ * Programme aspect BLUETOOTH
+ **************************************************
+ * NOM : HC-05
+ * PINCODE : 1234
+ * 
+ */
+
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial Bluetooth(4,5);   //Port D4 ?
